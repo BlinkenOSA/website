4226ceb9a9f8c696bcee82e187b20b5f1c322d0f
Josh <jozsef.bone@gmail.com>
2024-02-01 21:17:21 +0100

    Added elements

---
+++ src/components/IndexPage/CredoPanel.js
@@ -0,0 +1,29 @@
+import style from "./CredoPanel.module.scss"
+import archivumLogo from "../../../public/icons/credo/archivum.svg"
+import Image from "next/image";
+
+const CredoPanel = () => {
+    return (
+        <div className={style.Panel}>
+            <div className={style.Card}>
+                <div className={style.Text}>
+                    <h1>We are the Archivum</h1>
+                    <span>
+                        The Blinken OSA at Central European University is a dynamic archival institution focused on
+                        Cold War and human rights collections, employing innovative practices.
+                    </span>
+                </div>
+                <div className={style.Logo}>
+                    <Image
+                        priority
+                        src={archivumLogo}
+                        height={'600px'}
+                        alt="Blinken OSA Archivum"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CredoPanel;
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,8 @@ import EventsDivider from "@/components/IndexPage/EventsDivider";
 import EventCard from "@/components/Cards/EventCard";
 import CatalogPanel from "@/components/IndexPage/CatalogPanel";
 import SectionPanel from "@/components/IndexPage/SectionPanel";
+import CredoPanel from "@/components/IndexPage/CredoPanel";
+import CollectionCard from "@/components/Cards/CollectionCard";
 
 const IndexPage = () => {
   return (
@@ -240,6 +242,46 @@ const IndexPage = () => {
                   />
               </Col>
           </SectionPanel>
+          <div style={{height: '40px'}}/>
+          <CredoPanel />
+          <div style={{height: '40px'}}/>
+          <SectionPanel title={'Collection Highlights'}>
+              <Col xs={4}>
+                  <CollectionCard
+                      itemNumber={1285}
+                      title={'1956 Digital Archive'}
+                      author={'Darius Krolikowski'}
+                      description={'The 1956 Digital Archive is a collection of English-language text documents, ' +
+                          'photographs, and films on the 1956 Revolution in Hungary and its reception abroad.'}
+                      image={'https://fortepan.download/file/fortepan-eu/480/fortepan_23685.jpg'}
+                      types={['audio', 'movingImage', 'textual', 'stillImage']}
+                  />
+              </Col>
+              <Col xs={4}>
+                  <CollectionCard
+                      itemNumber={120}
+                      title={'1956 Hungarian Refugees in the United States'}
+                      author={'Darius Krolikowski'}
+                      description={'In 2016 with the generous support of the Blinken family, the archives extended ' +
+                          'the scope of its research to other archives in the United States that also possess ' +
+                          'relevant, still largely unexplored records on the 1956 Hungarian refugees.'}
+                      image={'http://www.refugees1956.org/wp-content/uploads/2016/11/nara-refugees01.jpg'}
+                      types={['textual', 'stillImage']}
+                  />
+              </Col>
+              <Col xs={4}>
+                  <CollectionCard
+                      itemNumber={86}
+                      title={'David Rohde Collection on Srebrenica'}
+                      author={'Darius Krolikowski'}
+                      description={'This research collection contains correspondence, eyewitness accounts, transcripts of phone ' +
+                          'conversations and other intelligence material, reports of human rights NGO, interoffice memos'}
+                      image={'https://osaarchivum.org/files/tmp/0039_rohde.jpg?1704815330'}
+                      types={['textual']}
+                  />
+              </Col>
+          </SectionPanel>
+          <div style={{height: '40px'}}/>
       </>
   )
 }
