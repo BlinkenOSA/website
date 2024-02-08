ee2858db88b56c8e8b8d14992fc81e1e32f34ad0
JoshBone <jozsef.bone@gmail.com>
2024-02-08 17:53:57 +0100

    Buttons addition

--- src/pages/design/buttons.js
+++ src/pages/design/buttons.js
@@ -1,9 +1,11 @@
 import style from "@/pages/pages.module.scss";
 import Button from "@/components/Button/Button";
 import {IconGeneralRight} from "@/components/Icon/Icon";
+import {Container} from "react-bootstrap";
 
 const ButtonsPage = () => {
 	return (
+		<Container>
 		<div className={style.PageWithMenuOpen}>
 			<h1>Buttons</h1><br/><br/>
 			<h3>Neutral</h3><br/>
@@ -14,6 +16,14 @@ const ButtonsPage = () => {
 				<Button type={'secondary'} size={'medium'} color={'neutral'} disabled={true}>Secondary</Button>
 				<Button type={'tertiary'} size={'medium'} color={'neutral'}>Tertiary</Button>
 				<Button type={'tertiary'} size={'medium'} color={'neutral'} disabled={true}>Tertiary</Button>
+			</div><br/>
+			<div style={{display: 'flex', gap: '16px'}}>
+				<Button type={'primary'} size={'large'} color={'neutral'}>Primary</Button>
+				<Button type={'primary'} size={'large'} color={'neutral'} disabled={true}>Primary</Button>
+				<Button type={'secondary'} size={'large'} color={'neutral'}>Secondary</Button>
+				<Button type={'secondary'} size={'large'} color={'neutral'} disabled={true}>Secondary</Button>
+				<Button type={'tertiary'} size={'large'} color={'neutral'}>Tertiary</Button>
+				<Button type={'tertiary'} size={'large'} color={'neutral'} disabled={true}>Tertiary</Button>
 			</div><br/>
 			<h3>Mustard</h3><br/>
 			<div style={{display: 'flex', gap: '16px'}}>
@@ -85,6 +95,8 @@ const ButtonsPage = () => {
 			</div><br/><br/>
 			<h3>Icons</h3><br/>
 			<div style={{display: 'flex', gap: '16px'}}>
+				<Button type={'primary'} size={'medium'} color={'neutral'} isIcon={true}><IconGeneralRight/></Button>
+				<Button type={'primary'} size={'medium'} color={'neutral'} isIcon={true} disabled={true}><IconGeneralRight/></Button>
 				<Button type={'primary'} size={'medium'} color={'mustard'} isIcon={true}><IconGeneralRight/></Button>
 				<Button type={'primary'} size={'medium'} color={'mustard'} isIcon={true} disabled={true}><IconGeneralRight/></Button>
 				<Button type={'primary'} size={'medium'} color={'orange'} isIcon={true}><IconGeneralRight/></Button>
@@ -96,6 +108,8 @@ const ButtonsPage = () => {
 
 			</div><br/>
 			<div style={{display: 'flex', gap: '16px'}}>
+				<Button type={'primary'} size={'large'} color={'neutral'} isIcon={true}><IconGeneralRight/></Button>
+				<Button type={'primary'} size={'large'} color={'neutral'} isIcon={true} disabled={true}><IconGeneralRight/></Button>
 				<Button type={'primary'} size={'large'} color={'mustard'} isIcon={true}><IconGeneralRight/></Button>
 				<Button type={'primary'} size={'large'} color={'mustard'} isIcon={true} disabled={true}><IconGeneralRight/></Button>
 				<Button type={'primary'} size={'large'} color={'orange'} isIcon={true}><IconGeneralRight/></Button>
@@ -106,6 +120,7 @@ const ButtonsPage = () => {
 				<Button type={'primary'} size={'large'} color={'sage'} isIcon={true} disabled={true}><IconGeneralRight/></Button>
 			</div><br/><br/>
 		</div>
+		</Container>
 	)
 }
 
