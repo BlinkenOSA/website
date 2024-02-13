3b5bf6ab23b16f9eeea76de5a04cc847b1ebf381
JoshBone <jozsef.bone@gmail.com>
2024-02-13 12:25:30 +0100

    Measurements correction

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -5,52 +5,56 @@ import {SocialIconFB, SocialIconInsta, SocialIconYT} from "@/components/Icon/Soc
 
 const Footer = () => {
 	return (
-		<div className={style.FooterWrapper}>
-			<Container>
-				<Row>
-					<Col xs={3}>
-						<Logo mode={'light'} height={30} />
-					</Col>
-					<Col xs={6} style={{paddingRight: '20px'}}>
-						<div className={style.InfoWrapperLeft}>
-							<div className={style.Label}>Contact</div>
-							<div className={style.Info}>
-								For archival matters: info@osaarchivum.org<br/>
-								For public programs: comms@ceu.edu<br/>
-								<br/>
-								Address: Arany János u. 32, 1051  Budapest, Hungary<br/>
-								Tel: +36-1-327-3250
+		<div className={style.FooterContainer}>
+			<div className={style.FooterWrapper}>
+				<Container>
+					<Row>
+						<Col xs={3}>
+							<div className={style.Logo}>
+								<Logo mode={'light'} height={30} />
 							</div>
-						</div>
-						<br/>
-						<div className={style.InfoWrapperLeft}>
-							<div className={style.Label}>Opening</div>
-							<div className={style.Info}>
-								Galeria: Tue-Sun 10:00 - 18:00<br/>
-								Research Room: Mon-Fri 10:00 - 17:45
+						</Col>
+						<Col xs={6} style={{paddingRight: '20px'}}>
+							<div className={style.InfoWrapperLeft}>
+								<div className={style.Label}>Contact</div>
+								<div className={style.Info}>
+									For archival matters: info@osaarchivum.org<br/>
+									For public programs: comms@ceu.edu<br/>
+									<br/>
+									Address: Arany János u. 32, 1051  Budapest, Hungary<br/>
+									Tel: +36-1-327-3250
+								</div>
 							</div>
-						</div>
-					</Col>
-					<Col xs={3}>
-						<div className={style.InfoWrapperRight}>
-							<div className={style.Label}>About</div><br/>
-							<div className={style.Links}>
-								<ul>
-									<li>Privacy Policy</li>
-									<li>Terms & Conditions</li>
-									<li>Sitemap</li>
-								</ul>
+							<br/>
+							<div className={style.InfoWrapperLeft}>
+								<div className={style.Label}>Opening</div>
+								<div className={style.Info}>
+									Galeria: Tue-Sun 10:00 - 18:00<br/>
+									Research Room: Mon-Fri 10:00 - 17:45
+								</div>
 							</div>
-						</div>
-						<br/>
-						<div className={style.SocialIcons}>
-							<a href={'#'}><SocialIconFB /></a>
-							<a href={'#'}><SocialIconInsta /></a>
-							<a href={'#'}><SocialIconYT /></a>
-						</div>
-					</Col>
-				</Row>
-			</Container>
+						</Col>
+						<Col xs={3}>
+							<div className={style.InfoWrapperRight}>
+								<div className={style.Label}>About</div><br/>
+								<div className={style.Links}>
+									<ul>
+										<li>Privacy Policy</li>
+										<li>Terms & Conditions</li>
+										<li>Sitemap</li>
+									</ul>
+								</div>
+							</div>
+							<br/>
+							<div className={style.SocialIcons}>
+								<a href={'#'}><SocialIconFB /></a>
+								<a href={'#'}><SocialIconInsta /></a>
+								<a href={'#'}><SocialIconYT /></a>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</div>
 		</div>
 	)
 }
