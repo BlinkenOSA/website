354c9c234da4da5aadd93815448750ab2f745f08
Josh <jozsef.bone@gmail.com>
2024-03-06 23:05:31 +0100

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,21 +2,22 @@ import Header from "@/components/Layout/Header";
 import style from "@/components/Layout/Layout.module.scss";
 import Menu from "@/components/Menu/v2/Menu";
 import Footer from "@/components/Layout/Footer";
+import { MenuProvider} from "@/utils/context/MenuContext";
 
 const Layout = ({ children }) => {
     return (
-      <>
+        <MenuProvider>
             <div className={style.Header}>
                 <Header />
             </div>
-            <div className={`${style.Content} suisseIntlRegular`}>
+            <div className={`suisseIntlRegular`}>
                 {children}
                 <div className={style.Menu}>
                     <Menu />
                 </div>
             </div>
             <Footer />
-      </>
+        </MenuProvider>
     )
 }
 
