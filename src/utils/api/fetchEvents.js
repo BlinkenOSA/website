e8df639c8d01ac0d3cbb92f2004f5b30f002e48a
JoshBone <jozsef.bone@gmail.com>
2024-03-08 13:22:30 +0100

    Update fetchEvents.js

--- src/utils/api/fetchEvents.js
+++ src/utils/api/fetchEvents.js
@@ -6,7 +6,7 @@ export const fetchEventsFrontPage = () => {
         'sort[1]': 'createdAt:desc',
         'populate[0]': 'Image',
         'pagination[start]': 0,
-        'pagination[limit]': 3,
+        'pagination[limit]': 9,
         'fields[0]': 'Title',
         'fields[1]': 'CardText',
         'fields[2]': 'StartDate',
