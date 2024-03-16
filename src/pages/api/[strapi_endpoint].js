e7a2fecafe97a8f9a441a83ca422eded57bc85a8
Josh <jozsef.bone@gmail.com>
2024-03-16 14:23:00 +0100

    Update [strapi_endpoint].js

--- src/pages/api/[strapi_endpoint].js
+++ src/pages/api/[strapi_endpoint].js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
+    const { strapi_endpoint } = req.query
+
     // Authentication
     let headers = new Headers();
     headers.set('Authorization', `bearer ${process.env.STRAPI_API_KEY}`)
 
     const urlParams = new URLSearchParams(req.query)
 
-    const results = await fetch(`${process.env.STRAPI_URL}/collections?${urlParams}`, {
+    const results = await fetch(`${process.env.STRAPI_URL}/${strapi_endpoint}?${urlParams}`, {
         headers: headers
     }).then(r => r.json())
 
