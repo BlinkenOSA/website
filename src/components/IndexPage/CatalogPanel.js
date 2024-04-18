1319b38cbc2f818b1d2d03dd9e60ffa33763f126
JoshBone <jozsef.bone@gmail.com>
2024-04-18 13:47:50 +0200

    Update CatalogPanel.js

--- src/components/IndexPage/CatalogPanel.js
+++ src/components/IndexPage/CatalogPanel.js
@@ -5,16 +5,26 @@ import {Col, Container, Row} from "react-bootstrap";
 import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 import Typewriter from "@/components/Typewriter/Typewriter";
+import {useInView} from "framer-motion";
+import {useEffect, useMemo, useRef, useState} from "react";
 
 const CatalogPanel = () => {
     const { t } = useTranslation('index')
+
+    const ref = useRef(null)
+    const isInView = useInView(ref)
+    const [startAnimation, setStartAnimation] = useState(false)
+
+    useEffect(() => {
+        setStartAnimation(true)
+    })
 
     const handleEnter = (value) => {
         window.open(`https://catalog.osaarchivum.org/?query=${value}`)
     }
 
     return (
-        <div className={style.PanelWrapper}>
+        <div ref={ref} className={style.PanelWrapper}>
             <Image
                 priority={true}
                 alt={'Catalog background'}
@@ -31,13 +41,16 @@ const CatalogPanel = () => {
                             <SearchBox onPressEnter={handleEnter} />
                         </Col>
                         <Col xs={8}>
-                            <Typewriter text={'10,000 linear meters, 17,000 hours of audiovisual, and 15 TB of digital records, as well ' +
-                                'as 150,000 photographs, 6000+ documentary film titles and 19,000 library items on ' +
-                                'four main areas of interest: communism and Cold War, and their afterlives; ' +
-                                'human rights and social justice; ' +
-                                'Central European University; ' +
-                                'Open Society Foundations network.'}
-                            />
+                            {
+                                startAnimation &&
+                                <Typewriter text={'10,000 linear meters, 17,000 hours of audiovisual, and 15 TB of digital records, as well ' +
+                                    'as 150,000 photographs, 6000+ documentary film titles and 19,000 library items on ' +
+                                    'four main areas of interest: communism and Cold War, and their afterlives; ' +
+                                    'human rights and social justice; ' +
+                                    'Central European University; ' +
+                                    'Open Society Foundations network.'}
+                                />
+                            }
                         </Col>
                     </Row>
                 </Container>
