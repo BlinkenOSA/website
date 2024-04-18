fee4c4c7ee17c3525657fbda0dc793f3c8540f80
JoshBone <jozsef.bone@gmail.com>
2024-04-18 15:44:01 +0200

    Update Video.js

--- src/components/Content/elements/Video.js
+++ src/components/Content/elements/Video.js
@@ -2,13 +2,17 @@ import style from "./Video.module.scss";
 import YouTube from "react-youtube";
 import {Col, Row} from "react-bootstrap";
 
-const Video = (content) => {
+const Video = ({content}) => {
     const youtube = content['YouTube']
     const caption = content['Caption']
 
+    console.log(youtube)
+
     const renderContent = () => {
-        if (youtube !== null) {
-            const youtubeURL = youtube.replace('https://youtu.be/', '')
+        if (youtube) {
+            let youtubeURL = youtube.replace('https://youtu.be/', '')
+            youtubeURL = youtubeURL.replace('https://www.youtube.com/watch?v=', '')
+
             return (
                 <div className={style.YouTubePlayer}>
                     <YouTube videoId={youtubeURL} />
