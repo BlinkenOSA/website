cf2373df75cae3621abfea886e34fe5c6aa64c70
Josh <jozsef.bone@gmail.com>
2024-04-22 10:34:53 +0200

    Update CollectionCard.js

--- src/components/Cards/CollectionCard.js
+++ src/components/Cards/CollectionCard.js
@@ -3,6 +3,8 @@ import MaskedImage from "@/components/MaskedImage/MaskedImage";
 import {IconAudio, IconDocument, IconFilm, IconMovingImage, IconPhoto} from "@/components/Icon/Icon";
 import getImageUrl from "@/utils/content/getImageUrl";
 import truncateWithEllipses from "@/utils/truncateWithEllipsis";
+import {OverlayTrigger, Tooltip} from "react-bootstrap";
+import React from "react";
 
 const CollectionCard = ({data}) => {
     const title = data['Title']
@@ -13,16 +15,22 @@ const CollectionCard = ({data}) => {
     const contentTypes = data['ContentTypes'].join(', ')
     const slug = data['Slug']
 
+    const ToolTipStuff = ({ id, children, title }) => (
+        <OverlayTrigger overlay={<Tooltip id={id}>{title}</Tooltip>}>
+            <div>{children}</div>
+        </OverlayTrigger>
+    );
+
     const chooseIcon = (type, key) => {
       switch(type) {
         case 'Audio':
-          return <IconAudio key={key} size={'small'} color={'orange'} />
+            return <ToolTipStuff title={'Audio'}><IconAudio key={key} size={'small'} color={'orange'} /></ToolTipStuff>
         case 'Moving Image':
-          return <IconMovingImage key={key} size={'small'} color={'orange'} />
+            return <ToolTipStuff title={'Moving Image'}><IconMovingImage key={key} size={'small'} color={'orange'} /></ToolTipStuff>
         case 'Textual':
-          return <IconDocument key={key} size={'small'} color={'orange'} />
+            return <ToolTipStuff title={'Textual'}><IconDocument key={key} size={'small'} color={'orange'} /></ToolTipStuff>
         case 'Still Image':
-          return <IconPhoto key={key} size={'small'} color={'orange'} />
+            return <ToolTipStuff title={'Still Image'}><IconPhoto key={key} size={'small'} color={'orange'} /></ToolTipStuff>
       }
     }
 
