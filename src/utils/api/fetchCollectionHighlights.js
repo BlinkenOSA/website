b9feb191f4136114aea9c0ee50ec670be0552a8d
Josh <jozsef.bone@gmail.com>
2024-04-22 10:31:41 +0200

    Update fetchCollectionHighlights.js

--- src/utils/api/fetchCollectionHighlights.js
+++ src/utils/api/fetchCollectionHighlights.js
@@ -25,6 +25,7 @@ export const fetchCollectionHighlightsList = (max, contentType) => {
         'sort[0]': 'Title:asc',
         'populate[0]': 'Image',
         'pagination[start]': 0,
+        'pagination[limit]': 100,
         'fields[0]': 'Title',
         'fields[1]': 'CardText',
         'fields[2]': 'ContentTypes',
