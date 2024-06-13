c6233e34681d571e2c5dcbff8d8013d26898b7d4
JoshBone <jozsef.bone@gmail.com>
2024-06-13 12:59:38 +0200

    Update CredoPanel.js

--- src/components/IndexPage/CredoPanel.js
+++ src/components/IndexPage/CredoPanel.js
@@ -19,7 +19,7 @@ const CredoPanel = ({credoData}) => {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 6000,
         waitForAnimate: false,
         pauseOnHover: false,
         afterChange: handleChange
