8d674ec9cbbcf97a7360e577b082c66e15892473
Josh <jozsef.bone@gmail.com>
2025-10-23 18:44:26 +0200

    Updates with Staff Units

--- src/utils/filterValues/staffFilterValues.js
+++ src/utils/filterValues/staffFilterValues.js
@@ -6,8 +6,8 @@ export const staffFilterValues = [
 	{value: 'Founding Director', translationKey: 'staff__filter__founding_director'},
 	{value: 'IT', translationKey: 'staff__filter__it'},
 	{value: 'Library', translationKey: 'staff__filter__library'},
-	{value: 'Public Awareness Unit', translationKey: 'staff__filter__public_programs_and_outreach'},
-	{value: 'Records Management', translationKey: 'staff__filter__records_and_information_management'},
+	{value: 'Public Programs and Outreach', translationKey: 'staff__filter__public_programs_and_outreach'},
+	{value: 'Records and Information Management', translationKey: 'staff__filter__records_and_information_management'},
 	{value: 'Reference Services', translationKey: 'staff__filter__reference_services'},
 	{value: 'Research', translationKey: 'staff__filter__research'},
 	{value: 'Verzio Filmfestival', translationKey: 'staff__filter__verzio'}
