81e9b4000881712724a662eacd73d062b70b3748
Josh <jozsef.bone@gmail.com>
2026-07-01 16:13:19 +0200

    Acting Director addition

--- src/utils/filterValues/staffFilterValues.js
+++ src/utils/filterValues/staffFilterValues.js
@@ -1,14 +1,14 @@
 export const staffFilterValues = [
+	{value: 'Acting Director', translationKey: 'staff__filter__acting_director'},
 	{value: 'Administration', translationKey: 'staff__filter__administration'},
 	{value: 'Archival Programs', translationKey: 'staff__filter__archival_programs'},
 	{value: 'Audiovisual Unit', translationKey: 'staff__filter__av_unit'},
 	{value: 'Chief Archivist', translationKey: 'staff__filter__chief_archivist'},
-	{value: 'Founding Director', translationKey: 'staff__filter__founding_director'},
 	{value: 'IT', translationKey: 'staff__filter__it'},
 	{value: 'Library', translationKey: 'staff__filter__library'},
 	{value: 'Public Programs and Outreach', translationKey: 'staff__filter__public_programs_and_outreach'},
 	{value: 'Records and Information Management', translationKey: 'staff__filter__records_and_information_management'},
-	{value: 'Reference Services', translationKey: 'staff__filter__reference_services'},
+	{value: 'Reference Service', translationKey: 'staff__filter__reference_services'},
 	{value: 'Research', translationKey: 'staff__filter__research'},
 	{value: 'Verzio Filmfestival', translationKey: 'staff__filter__verzio'}
 ]
